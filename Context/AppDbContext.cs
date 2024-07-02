721029ca6e16f7f2ff213e1f1c62b351bbbc879f
Leandro Sabo <lu1gaq.ar@gmail.com>
2024-07-02 02:48:30 -0300

    Cambiar el listado de archivos para ignorar

---
+++ Context/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using barAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace barAPI.Context
+{
+    public class AppDbContext: DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options): base(options)
+        {
+            
+        }
+
+        public DbSet<Table> Tables { get; set; }
+    }
+}
