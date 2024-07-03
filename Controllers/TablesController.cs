648f4da47c957af7c4db7b199e6a518d3043c054
Leandro Sabo <lu1gaq.ar@gmail.com>
2024-07-03 01:18:28 -0300

    Crear controlador para las mesas con el Entity mágico

---
+++ Controllers/TablesController.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using barAPI.Context;
+using barAPI.Models;
+
+namespace barAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TablesController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public TablesController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Tables
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Table>>> GetTables()
+        {
+            return await _context.Tables.ToListAsync();
+        }
+
+        // GET: api/Tables/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Table>> GetTable(string id)
+        {
+            var table = await _context.Tables.FindAsync(id);
+
+            if (table == null)
+            {
+                return NotFound();
+            }
+
+            return table;
+        }
+
+        // PUT: api/Tables/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutTable(string id, Table table)
+        {
+            if (id != table.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(table).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!TableExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Tables
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Table>> PostTable(Table table)
+        {
+            _context.Tables.Add(table);
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateException)
+            {
+                if (TableExists(table.Id))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtAction("GetTable", new { id = table.Id }, table);
+        }
+
+        // DELETE: api/Tables/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteTable(string id)
+        {
+            var table = await _context.Tables.FindAsync(id);
+            if (table == null)
+            {
+                return NotFound();
+            }
+
+            _context.Tables.Remove(table);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool TableExists(string id)
+        {
+            return _context.Tables.Any(e => e.Id == id);
+        }
+    }
+}
