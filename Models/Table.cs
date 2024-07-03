4511d776df52ff22a64acc4f680b3eb22f4a5737
Leandro Sabo <lu1gaq.ar@gmail.com>
2024-07-03 01:20:43 -0300

    Cambiar el tipo de dato en la clave primaria de Table

--- Models/Table.cs
+++ Models/Table.cs
@@ -2,7 +2,7 @@
 {
     public class Table
     {
-        public string Id { get; set; }
+        public int Id { get; set; }
         public string Name { get; set; }
         public required int Chairs { get; set; }
     }
